ab87b8539f9d7381f35661c14d02994bf8963bd1
Sandhya <sandhyadevadiga8197@gmail.com>
2024-08-03 14:15:49 +0530

    first_commit

---
+++ calculator/src/App.jsx
@@ -0,0 +1,48 @@
+// import ButtonsContainer from "./components/ButtonsContainer";
+// import Display from "./components/Display";
+// import styles from "./App.module.css"
+// // import "./App.css"
+// function App() {
+  
+//   return (
+//     <div className={styles.calculator}>
+//       <Display></Display>
+//       <ButtonsContainer></ButtonsContainer>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
+import Display from "./components/Display";
+import ButtonsContainer from "./components/ButtonsContainer";
+import styles from "./App.module.css";
+import { useState } from "react";
+
+function App() {
+  const [calVal, setCalVal] = useState("");
+
+  const onButtonClick = (buttonText) => {
+    if (buttonText === "C") {
+      setCalVal("");
+    } else if (buttonText === "=") {
+      const result = eval(calVal);
+      setCalVal(result);
+    } else {
+      const newDisplayValue = calVal + buttonText;
+      setCalVal(newDisplayValue);
+    }
+  };
+
+  return (
+    
+    <div className={styles.calculator}>
+      <Display displayValue={calVal}></Display>
+      <ButtonsContainer onButtonClick={onButtonClick}></ButtonsContainer>
+    </div>
+    
+  );
+}
+
+export default App;
